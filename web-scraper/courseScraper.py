cde6eebe53076c58956d44ee54d10d21ad4d9cc4
Randolf <knh071@usask.ca>
2025-10-21 09:17:37 -0600

    Created a function what extracts course numbers

--- web-scraper/courseScraper.py
+++ web-scraper/courseScraper.py
@@ -1,7 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
 
+
+def get_class_num(title):
+    """
+    Takes in a title, splits the array based on the white spaces
+    then returns only the class number.
+
+    title: A string where the second word is the class number
+    """
+    # Turns title into an array
+    titleParts = title.strip().split()
+
+    num = titleParts[1].split('.')[0] #No period, or extra numbers
+    return num
+
+
+
 def fetch_courses(courseSubject):
+    """
+    Takes in a course subject such as CMPT, and extracts all the data
+    related to that subject from the usask course catalogue page. 
+
+    courseSubject: A subject that is in all capitals
+    
+    """
+    
     url = f"https://catalogue.usask.ca/?subj_code={courseSubject}&cnum="
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
@@ -18,7 +42,14 @@ def fetch_courses(courseSubject):
             continue
         else:
             title = course.find('h4').find('a').text
-        print(title)
+        
+        # Class number
+        number = get_class_num(title)
+
+
+
+
+
 
         
 
