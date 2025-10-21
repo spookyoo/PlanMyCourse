037d8c51c207dd376f6acd7dc82614088573297e
Randolf <knh071@usask.ca>
2025-10-21 01:22:43 -0600

    found title of courses

--- web-scraper/courseScraper.py
+++ web-scraper/courseScraper.py
@@ -1,8 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
 
-def fetch_courses(coruseSubject):
-    print(coruseSubject)
+def fetch_courses(courseSubject):
+    url = f"https://catalogue.usask.ca/?subj_code={courseSubject}&cnum="
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    courses = []
+    course_elements = soup.find_all('div')
+
+    for course in course_elements:
+
+        # Finding the title of the course
+        if course.find('h4') is None:
+            continue
+        elif course.find('h4').find('a') is None:
+            continue
+        else:
+            title = course.find('h4').find('a').text
+        print(title)
+
+        
+
 
 def main():
     fetch_courses('CMPT')
