7ef6d674707a398821738ddf4eb32e0450764ff4
Randolf <knh071@usask.ca>
2025-10-20 20:36:03 -0600

    Created dummy functions

--- web-scraper/courseScraper.py
+++ web-scraper/courseScraper.py
@@ -1,2 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
+
+def fetch_courses(coruseSubject):
+    print(coruseSubject)
+
+def main():
+    fetch_courses('CMPT')
+
+if __name__=="__main__":
+    main()
