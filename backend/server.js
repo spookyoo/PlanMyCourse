fcc68332a164504bcc169b4d3bf9f74f827e8332
Randolf <knh071@usask.ca>
2025-10-26 18:59:51 -0600

    Created a CoursesAdded table

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const coursesRoutes = require('./routes/courses');
 const userRoutes = require('./routes/users');
 const coursesAddedRoutes = require('./routes/coursesadded');
+const { createCoursesAddedTable } = require('./models/users');
 
 const app = express();
 app.use(express.json());
@@ -10,6 +11,8 @@ app.use(express.json());
 app.use('/courses', coursesRoutes);
 app.use('/users', userRoutes);
 app.use('/coursesadded', coursesAddedRoutes);
+
+createCoursesAddedTable();
 
 app.listen(3001, () => {
     console.log("Server is running on port 3001");
