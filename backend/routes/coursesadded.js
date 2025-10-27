431568abc6d3fea5424d9e94e6ee2857effc4be2
Randolf <knh071@usask.ca>
2025-10-26 20:27:34 -0600

    added post router and changed name of endpoint

--- backend/routes/coursesadded.js
+++ backend/routes/coursesadded.js
@@ -2,9 +2,9 @@ const express = require('express');
 const connectMade = require('../config.js');
 const router = express.Router();
 
-router.get('/added', (req, res) => {
+router.get('/', (req, res) => {
     try{
-        connectMade.query('SELECT * FROM Coursesadded', (err, results) => {
+        connectMade.query('SELECT * FROM CoursesAdded', (err, results) => {
         if(err){
             console.error('There has been a query error.', err);
             res.status(500).send('There has been an error with getting the coursesadded table.');
@@ -17,5 +17,18 @@ router.get('/added', (req, res) => {
         console.error('There seems to be no way of getting the courses that were added from the coursesadded table overall.');
     }
 });
+
+router.post('/', (req, res) => {
+    const { courseId } = req.body;
+    const query = `INSERT INTO CoursesAdded (courseId) Values (?)`;
+    connectMade.query(query, [courseId], (err, result) => {
+        if(err){
+            res.send("Did not insert properly to CoursesAdded table")
+            return;
+        }
+        res.json(req.body)
+    })
+
+})
 
 module.exports = router;
