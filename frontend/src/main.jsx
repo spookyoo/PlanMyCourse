286339e3eed354d3ae281ff35461820307d28ffc
spookyoo <melvincruz07@gmail.com>
2025-10-27 19:41:23 -0600

    Initialized separate Navbar directory
    - Initialized Navbar.css
    - Imported Navbar.css globally

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import App from './app/App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import './app/App.css'
+import './components/Navbar/Navbar.css'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
