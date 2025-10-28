fa687ce633d30b0fb0d480ecd43488c68510a176
spookyoo <melvincruz07@gmail.com>
2025-10-27 19:03:02 -0600

    Implemented global navbar
    - Initialized Navbar component
    - Removed default React CSS
    - Imported App.css into main.jsx
    - Added Navbar css to App.css
    - Refactored HomePage files to implement Navbar component

--- frontend/src/app/pages/HomePage.jsx
+++ frontend/src/app/pages/HomePage.jsx
@@ -1,26 +1,12 @@
 import './HomePage.css'
 
+import Navbar from '../../components/Navbar'
+
 function HomePage() {
 
   return (
     <>
-        <nav className='navbar'>
-          <div className='title-section'>
-            <h2>PlanMyCourse</h2>
-          </div>
-          <div className='nav-links'>
-            <ul>
-              <li>Home</li>
-              <li>Courses</li>
-              <li>Planner</li>
-              <li>Graph</li>
-            </ul>
-            <div className='profile-button'>
-              <button>Profile</button>
-            </div>
-          </div>
-        </nav>
-    
+        <Navbar />
         <div className="main-container">
             <div className="website-name">
               <h1>PlanMyCourse</h1>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import App from './app/App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
+import './app/App.css'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
