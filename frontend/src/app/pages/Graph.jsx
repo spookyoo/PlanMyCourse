c0cf8be85502c7723ebfd187ec62fa298945af13
antoinetterubia <mar092@usask.ca>
2025-11-01 21:00:34 -0600

    reverting changes

--- frontend/src/app/pages/Graph.jsx
+++ frontend/src/app/pages/Graph.jsx
@@ -3,18 +3,6 @@ import Navbar from "../../components/Navbar/Navbar"
 import './Graph.css'
 
 function Graph() {
-    const [searchQuery, setSearchQuery] = useState("");
-    const [filteredCourses, setFilteredCourses] = useState([]);
-
-    const courses = [
-        { course_id: "CS101", course_name: "Introduction to Computer Science" },
-        { course_id: "MATH201", course_name: "Calculus II" },
-        { course_id: "ENG150", course_name: "English Literature" },
-    ];
-
-    const handleSearch = () => {
-        const results
-    }
     return (
         <>
         <Navbar />
