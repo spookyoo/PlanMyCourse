19bd6f7811f640761c3017bfc52d7d14449ea099
antoinetterubia <mar092@usask.ca>
2025-11-01 20:40:19 -0600

    attempt to merge from main

--- backend/config.js
+++ backend/config.js
@@ -4,10 +4,10 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const connectMade = mysql.createConnection({
-    host: process.env.DB_HOST, 
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
+    host: process.env.DB_HOST || 'localhost', 
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASS || 'ar.rubia',
+    database: process.env.DB_NAME || 'coursesdb',
     port: process.env.DB_PORT
 });
 
