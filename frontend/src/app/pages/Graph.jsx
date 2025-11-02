2ffd701bf320b8ca91aebe99525396ebc2a6d8ab
antoinetterubia <mar092@usask.ca>
2025-11-01 20:54:24 -0600

    removed search attempt

--- frontend/src/app/pages/Graph.jsx
+++ frontend/src/app/pages/Graph.jsx
@@ -1,35 +1,8 @@
 import React from "react";
-<<<<<<< HEAD
-import Navbar from '../../components/Navbar/Navbar'
-
-
-function GraphPage () {
-    <>
-    <Navbar />
-
-    <div className="seacrch-bar">
-        <input type="text" placeholder="Enter a class.."/>
-    </div>
-
-    </>
-}
-=======
 import Navbar from "../../components/Navbar/Navbar"
 import './Graph.css'
 
 function Graph() {
-    const [searchQuery, setSearchQuery] = useState("");
-    const [filteredCourses, setFilteredCourses] = useState([]);
-
-    const courses = [
-        { course_id: "CS101", course_name: "Introduction to Computer Science" },
-        { course_id: "MATH201", course_name: "Calculus II" },
-        { course_id: "ENG150", course_name: "English Literature" },
-    ];
-
-    const handleSearch = () => {
-        const results
-    }
     return (
         <>
         <Navbar />
@@ -44,4 +17,3 @@ function Graph() {
 }
 
 export default Graph
->>>>>>> main
