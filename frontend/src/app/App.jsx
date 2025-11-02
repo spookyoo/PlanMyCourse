552f6de8aea9e30debcba3ea91ee0ff24037b88a
antoinetterubia <mar092@usask.ca>
2025-11-01 20:37:31 -0600

    barebone graph page for now

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Home from './pages/HomePage'
 import Planner from './pages/PlannerPage'
+import Graph from './pages/Graph'
 function App() {
 
   return (
     <Routes>
       <Route path='/' element={<Home />}/>
       <Route path='/planner' element={<Planner />}/>
+      <Route path='/graph' element={<Graph />}/>
     </Routes>
   )
 }
