82d5e930359eb5e69faec12dffd56f0823c4fd28
spookyoo <melvincruz07@gmail.com>
2025-11-03 17:36:48 -0600

    fixed bug
    - charles bug

--- frontend/src/app/pages/HomePage.jsx
+++ frontend/src/app/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import './HomePage.css'
-import Redirect from '../../components/SearchCourse/Redirect'
+import Redirect from '../../components/SearchBar/Redirect'
 
 function HomePage() {
   return (
