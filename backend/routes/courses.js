05f89e3c1fe2a44a37dac91587a0ebfb31465d6d
charles gamboa <kgv201@usask.ca>
2025-11-03 18:48:25 -0600

    added whatever melvin needed

--- backend/routes/courses.js
+++ backend/routes/courses.js
@@ -20,6 +20,18 @@ router.get('/', (req, res) => {
     }
 });
 
+//To get that of the a course's by name via parameter
+router.get('/:name', (req,res) => {
+    connectMade.query(`SELECT * FROM Courses WHERE class_name = ?`, [req.params.name], (err, results) => {
+        if(err){
+            console.error('There has been an error getting the course from the courses table.');
+            res.status(500).send('Seems to be that of course to be selected is not at all being seen in the courses table.');
+            return;
+        }
+        res.json(results);
+    });
+});
+
 //To get that of the a course's by name, subject or level
 router.get('/search', (req,res) => {
 
@@ -34,7 +46,7 @@ router.get('/search', (req,res) => {
                   OR subject LIKE ?
                   OR courseId LIKE ?
                   OR FLOOR(number / 100) * 100 = ?
-                  `
+                  `;
     
     var searchValue = `%${searchTerm}`;
     if (Number(searchTerm)) {
