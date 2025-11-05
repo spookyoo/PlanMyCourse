ef3b300b8e3432a3bd327195f92131f94769783f
spookyoo <melvincruz07@gmail.com>
2025-11-05 10:19:16 -0600

    Implemented course info
    - Displays prerequisites, description, and notes
    - Added css

--- frontend/src/app/pages/CoursePage.jsx
+++ frontend/src/app/pages/CoursePage.jsx
@@ -1,15 +1,29 @@
-import './CoursePage.css'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import './CoursePage.css';
 
 function CoursePage() {
     const {courseId} = useParams()
     const [course, setCourse] = useState({});
+    const [courseNotes, setCourseNotes] = useState("");
+    const [coursePrerequisites, setPrerequisites] = useState("");
+
+    function processNotes(rawNotes) {
+        const cleanData = rawNotes.split("Prerequisite(s):")[1] || "";
+
+        const [prerequisites, notes] = cleanData.split("Note:");
+        
+        return { prerequisites, notes };
+    }
     useEffect(() => {
         axios.get(`http://localhost:3001/courses/${courseId}`)
         .then(response => {
-            setCourse(response.data[0]);
+            const courseData = response.data[0];
+            setCourse(courseData);
+            const { prerequisites, notes } = processNotes(courseData.notes);
+            setPrerequisites(prerequisites);
+            setCourseNotes(notes);
         })
         .catch(error => {
             console.error("Error fetching by id", error)
@@ -20,7 +34,25 @@ function CoursePage() {
         <div className="course-header">
             <h1>{course.title}</h1>
             <hr></hr>
-            
+        </div>
+        <div className="course-info">
+            <h3>Description:</h3>
+            <span className="course-description">{course.description}</span>
+            <h5>Prerequisite(s):</h5>      
+            <span className="course-prerequisites">{coursePrerequisites}</span>
+            <h5>Notes:</h5>
+            <span className="course-notes">{courseNotes}</span>
+            <div className="course-buttons">
+                {/* <div className=""></div> */}
+            </div>
+        </div>
+        <div className="course-rating">
+            <div className="course-statistics">
+            </div>
+            <div className="divider"></div>
+            <div className="course-new-review"></div>
+        </div>
+        <div className="course-reviews">
         </div>
     </div>
     )
