ee3fc4df1c0dea354dba1c270bc4086c9a1ea55b
Runn077 <rupu0777@gmail.com>
2025-11-15 02:00:05 -0600

    seperated server.js file into app.js for testing purposes, and for a cleaner structure

---
+++ backend/app.js
@@ -0,0 +1,27 @@
+// app.js
+const express = require('express');
+
+// route imports
+const coursesRoutes = require('./routes/courses');
+const prerequisitesRoutes = require('./routes/prerequisites');
+const userRoutes = require('./routes/users');
+const coursesAddedRoutes = require('./routes/coursesadded');
+const authRoutes = require('./routes/auth');
+const reviewsMadeRoutes = require('./routes/reviewsmade');
+
+const cors = require('cors');
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// routes
+app.use('/courses', coursesRoutes);
+app.use('/prerequisites', prerequisitesRoutes);
+app.use('/users', userRoutes);
+app.use('/coursesadded', coursesAddedRoutes);
+app.use('/reviewsmade', reviewsMadeRoutes);
+app.use('/auth', authRoutes);
+
+module.exports = app;
