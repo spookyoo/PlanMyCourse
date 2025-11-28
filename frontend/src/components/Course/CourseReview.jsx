6a7f4d97053d6895a33c2117f8f95081f9bf7b12
spookyoo <melvincruz07@gmail.com>
2025-11-27 21:00:24 -0600

    Changes to CourseReview component
    - Added classes to html elements

--- frontend/src/components/Course/CourseReview.jsx
+++ frontend/src/components/Course/CourseReview.jsx
@@ -4,10 +4,13 @@ function CourseReview( {username, message, rating, timestamp} ) {
     return (   
         <div className="course-review">
             <div className="review-header">
-                <h4>{username}</h4>
-                <p>{timestamp}</p>
+                <div className="review-userData">
+                    <p className="review-username">{username}</p>
+                    <p></p>
+                </div>
+                <p className="review-timestamp">{timestamp}</p>
             </div>
-            <span>{message}</span>
+            <span className="review-message">{message}</span>
 
         </div>
     );
