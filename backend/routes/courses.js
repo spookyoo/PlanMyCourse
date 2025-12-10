0c642d79e745768f46763e4dc598329704ef247a
charles gamboa <kgv201@usask.ca>
2025-12-09 22:03:16 -0600

    fixed old files

--- backend/routes/courses.js
+++ backend/routes/courses.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const connectMade = require('../config.js');
 const router = express.Router();
+const { verifyToken } = require('../middleware/authMiddleware.js');
 
-//To get all courses
+// GET
+// Get all courses
 router.get('/', (req, res) => {
     try{
-        connectMade.query('SELECT * FROM Courses', (err, results) => {
+        const limit = parseInt(req.query.limit) || 20;
+        const offset = parseInt(req.query.offset) || 0;
+        connectMade.query('SELECT * FROM Courses LIMIT ?, ?', [offset, limit], (err, results) => {
         if(err){
             console.error('There has been a query error.', err);
             res.status(500).send('There has been an error with getting the courses table.');
@@ -20,40 +24,49 @@ router.get('/', (req, res) => {
     }
 });
 
-//To get that of the a course's by name, subject or level
+// GET
+// Get a course by name, subject or level
 router.get('/search', (req,res) => {
-
     const searchTerm = req.query.term;
+    const limit = parseInt(req.query.limit) || 20;
+    const offset = parseInt(req.query.offset) || 0;
     if (!searchTerm) {
         res.status(400).json({error: "Search term is required"})
         return
     }
 
-    var query = `SELECT * FROM Courses WHERE 
-                    class_name LIKE ? 
-                  OR subject LIKE ?
-                  OR courseId LIKE ?
-                  OR FLOOR(number / 100) * 100 LIKE ?
-                  OR title LIKE ?
-                  `;
-    
-    var searchValue = `%${searchTerm}%`;
-    if (Number(searchTerm)) {
-        searchValue = `%${searchTerm}%`
+    const isNumber = !isNaN(Number(searchTerm));
+    let query = '';
+    let params = [];
+
+    if (isNumber) {
+        // search by level
+        query = `SELECT * FROM Courses WHERE FLOOR(number / 100) * 100 = ? LIMIT ?, ?`;
+        params = [Number(searchTerm), offset, limit];
+    } else {
+        // search by subject or class_name or title
+        query = `SELECT * FROM Courses WHERE 
+        class_name LIKE ? OR 
+        subject LIKE ? OR
+        title LIKE ?
+        LIMIT ?, 
+        ?`;
+        params = [`%${searchTerm}%`, `%${searchTerm}`, `%${searchTerm}%`, offset, limit];
     }
 
-    connectMade.query(query, [searchValue, searchValue, searchValue, searchValue, searchValue], (err, results) => {
+    connectMade.query(query, params, (err, results) => {
         if(err){
-            console.error('There has been an error getting the course from the courses table.');
-            res.status(500).send('Seems to be that of course to be selected is not at all being seen in the courses table.');
+            console.error('Error getting courses', err);
+            res.status(500).send('Error fetching courses.');
             return;
         }
         res.json(results);
     });
 });
 
-//To get that of the a course's by name via parameter
-router.get('/:name', (req,res) => {
+// GET
+// Get by class_name
+router.get('/name/:name', (req,res) => {
     connectMade.query(`SELECT * FROM Courses WHERE class_name = ?`, [req.params.name], (err, results) => {
         if(err){
             console.error('There has been an error getting the course from the courses table.');
@@ -64,9 +77,10 @@ router.get('/:name', (req,res) => {
     });
 });
 
-//To get that of the a course's by name via parameter
-router.get('/:id', (req,res) => {
-    connectMade.query(`SELECT * FROM Courses WHERE courseId = ?`, [req.params.name], (err, results) => {
+// GET
+// Get by courseId
+router.get('/id/:id', (req,res) => {
+    connectMade.query(`SELECT * FROM Courses WHERE courseId = ?`, [req.params.id], (err, results) => {
         if(err){
             console.error('There has been an error getting the course from the courses table.');
             res.status(500).send('Seems to be that of course to be selected is not at all being seen in the courses table.');
@@ -76,9 +90,12 @@ router.get('/:id', (req,res) => {
     });
 });
 
+// GET
 //Get courses in that of an alphabetical order by that of their class name.
 router.get('/sort/alphabetical', (req, res) => {
-    connectMade.query(`SELECT * FROM Courses ORDER BY class_name ASC`, (err, results) => {
+    const limit = parseInt(req.query.limit) || 20;
+    const offset = parseInt(req.query.offset) || 0;
+    connectMade.query(`SELECT * FROM Courses ORDER BY class_name ASC LIMIT ?, ?`, [offset, limit], (err, results) => {
         if(err){
             console.error('There has been an error getting that of the courses for the course catalogue through that alphabetical.');
             res.status(500).send('Seems to be that of courses to be sorted through alphabetical order cannot be done.');
@@ -88,9 +105,12 @@ router.get('/sort/alphabetical', (req, res) => {
     });
 });
 
+// GET
 //Get courses in that of an alphabetical order by that of their class name.
 router.get('/sort/number', (req, res) => {
-    connectMade.query(`SELECT * FROM Courses ORDER BY number ASC`, (err, results) => {
+    const limit = parseInt(req.query.limit) || 20;
+    const offset = parseInt(req.query.offset) || 0;
+    connectMade.query(`SELECT * FROM Courses ORDER BY number ASC LIMIT ?, ?`, [offset, limit], (err, results) => {
         if(err){
             console.error('There has been an error getting that of the courses for the course catalogue through that of course number.');
             res.status(500).send('Seems to be that of courses to be sorted through course number order cannot be done.');
@@ -100,19 +120,30 @@ router.get('/sort/number', (req, res) => {
     });
 });
 
-//Get courses in which it sorts the catalogue in alphabetical order but have of the taken courses be displayed first and the untaken courses 
-// to be displayed down below.
-router.get('/sort/addedtoplanner', (req, res) => {
+// GET
+//Get courses in which it sorts the catalogue in alphabetical order but have of the courses that were added in the planner be displayed first and the 
+// courses that are not added to the planner be displayed before. 
+router.get('/sort/addedtoplanner', verifyToken, (req, res) => {
+    const limit = parseInt(req.query.limit) || 20;
+    const offset = parseInt(req.query.offset) || 0;
+    const userId = req.user.userId;
+
+    if(!userId){
+        return res.status(400).send('You must be a logged in user to sort that of the course catalogue based on the courses added to your planner.')
+    }
+
     const query = `
         SELECT 
             c.*,
             IF(ca.taken IS NULL, 0, 1) AS added
         FROM Courses c
-        LEFT JOIN CoursesAdded ca ON c.courseId = ca.courseId
+        LEFT JOIN CoursesAdded ca 
+            ON c.courseId = ca.courseId AND ca.userId = ?
         ORDER BY taken DESC, class_name ASC
-    `
+        LIMIT ?, ?
+    `;
 
-    connectMade.query(query, (err, results) => {
+    connectMade.query(query, [userId, offset, limit], (err, results) => {
         if(err){
             console.error('There has been an error getting that of the courses for the course catalogue that are seperated whether it is taken or not.');
             res.status(500).send('Seems to be that of courses to be sorted by the first half being taken courses and second half being untaken courses.');
